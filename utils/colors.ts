b07d8e8fb66a23b362fcf1d1cc4d5dd6b4b63255
rohit <rohit.880207@gmail.com>
2025-05-01 22:34:48 +0530

    updated colors

--- utils/colors.ts
+++ utils/colors.ts
@@ -2,6 +2,7 @@ import { Appearance } from "react-native";
 const theme = {
     dark: {
         text: '#ffffff',
+        secondText:'#3D3D3D',
         background: '#121212',
         secondBackground: '#1A1A1A',
         primary: '#00C896',
@@ -9,16 +10,22 @@ const theme = {
         skeletonBackground: '#1A1A1A',
         skeletonHighlight: '#333333',
         input:'white',
+        buttonBackground:'#EEEEEE',
+        negative:'#D85A32'
     },
     light: {
         input:'white',
         text: '#000000',
+        secondText:'#3D3D3D',
         secondBackground:'#F7F7F7',
         background: '#F6FAFB',
         primary: '#00C896',
         border: '#E9E9E9',
         skeletongBackground: '#F7F7F7',
-        skeletonHighlight: '#E0E0E0'
+        skeletonHighlight: '#E0E0E0',
+        buttonBackground:'#EEEEEE',
+        negative:'#D85A32'
+
 
     }
   };
