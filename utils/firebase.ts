2aa428d2326244d0a3f6246416a679293cc3a338
rohit <rohit.880207@gmail.com>
2025-05-04 11:32:27 +0530

    added environment variables

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth, setPersistence,inMemoryPersistence,browserLocalPersistence } f
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: "AIzaSyDvbgxaSIT0vP6M4_LZPgY6SC-siImJSXo",
-  authDomain: "todo-f16da.firebaseapp.com",
-  projectId: "todo-f16da",
-  storageBucket: "todo-f16da.firebasestorage.app",
-  messagingSenderId: "250983820341",
-  appId: "1:250983820341:web:0c9a40011274f3a2a9957c",
-  measurementId: "G-270W7Z23XG"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
